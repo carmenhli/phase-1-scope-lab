fc63e8c6fc51670824d45dc350472fc379aeff84
Carmen Li <cmnli95@gmail.com>
2023-11-12 17:11:40 -0500

    pusshing phase 1 scope lab carmen li

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+// 1
+var customerName = 'bob';
+console.log(customerName);
+
+// 2
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+// 3
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+// 4
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+// 5
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'someone else';
+}
